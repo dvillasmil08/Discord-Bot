e6ba5395f1c4dcd5b7a429edcfcbd9fb15e2d583
DanielVillasmil <dvillasmil08@gmail.com>
2022-06-21 20:18:18 -0400

    corrected code

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
+from genericpath import commonprefix
 import discord
 from discord.ext import commands
+import os
 
+client = commands.bot(commonprefix='$')
 
-bot = commands.Bot(command_prefix = '.')
+@client.event
+async def on_ready():
+    print('!!! Bot Is Online!!!\n')
 
-# bot replays with hello when calling .hello
-@bot.command()
-async def hello(ctx):
-    await ctx.reply('Hello!')
-
-
-@bot.command()
-async def hello(ctx):
-    await ctx.reply('Hello!')
-
-bot.run(config.token)
+client.run(os.getenv('TOKEN'))
